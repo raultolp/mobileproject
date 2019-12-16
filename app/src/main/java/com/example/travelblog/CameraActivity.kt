d5d4457438759cad8672d543df2079b912009a0b
raultolp <raul.tolp@gmail.com>
2019-12-16 03:34:12 +0200

    Camera first steps

---
+++ app/src/main/java/com/example/travelblog/CameraActivity.kt
@@ -0,0 +1,42 @@
+package com.example.travelblog
+
+import android.app.Activity
+import android.hardware.Camera
+import android.os.Bundle
+import android.widget.FrameLayout
+
+class CameraActivity : Activity() {
+
+    private var mCamera: Camera? = null
+    private var mPreview: CameraPreview? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance()
+
+        mPreview = mCamera?.let {
+            // Create our Preview view
+            CameraPreview(this, it)
+        }
+
+        // Set the Preview view as the content of our activity.
+        mPreview?.also {
+            val preview: FrameLayout = findViewById(R.id.camera_preview)
+            preview.addView(it)
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    fun getCameraInstance(): android.hardware.Camera? {
+        return try {
+            android.hardware.Camera.open() // attempt to get a Camera instance
+        } catch (e: Exception) {
+            // Camera is not available (in use or does not exist)
+            null // returns null if camera is unavailable
+        }
+    }
+
+}
--- app/src/main/java/com/example/travelblog/MainActivity.kt
+++ app/src/main/java/com/example/travelblog/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.travelblog
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -16,10 +19,15 @@ import com.google.android.gms.maps.model.Marker
 class MainActivity : AppCompatActivity() {
 
     lateinit var mapsActivity: MapsActivity
+    private val TAG = "MYAPI"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO: NB! LÕPUS KASUTA Camera.release()
+        checkCameraHardware(this)
+
         mapsActivity = MapsActivity()
         showMap()
     }
@@ -29,4 +37,16 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    /** Check if this device has a camera */
+    private fun checkCameraHardware(context: Context): Boolean {
+        return if (context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            val cameras = Camera.getNumberOfCameras()
+//            val info = Camera.getCameraInfo(0)
+            Log.i(TAG, "Cameras found: $cameras")
+            true
+        } else {
+            Log.i(TAG, "Camera NOT FOUND!!!")
+            false
+        }
+    }
 }
