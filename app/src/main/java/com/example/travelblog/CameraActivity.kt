174de406e29a9e76f5a39580fa8805fc0550f188
raultolp <raul.tolp@gmail.com>
2019-12-17 13:49:25 +0200

    Camera update

--- app/src/main/java/com/example/travelblog/CameraActivity.kt
+++ app/src/main/java/com/example/travelblog/CameraActivity.kt
@@ -1,15 +1,34 @@
-package com.example.travelblog
+package com.example.mycameraapi
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.hardware.Camera
+import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.SurfaceView
 import android.widget.Button
 import android.widget.FrameLayout
+import com.example.travelblog.CameraPreview
+import com.example.travelblog.R
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraActivity : Activity() {
 
     private var mCamera: Camera? = null
     private var mPreview: CameraPreview? = null
+    //    private var preview: SurfaceView? = null
+//    private var mediaRecorder: MediaRecorder? = null
+    val MEDIA_TYPE_IMAGE = 1
+    val MEDIA_TYPE_VIDEO = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +48,32 @@ class CameraActivity : Activity() {
             preview.addView(it)
         }
 
+        val picture = mPicture
+
         val captureButton: Button = findViewById(R.id.button_capture)
         captureButton.setOnClickListener {
             // get an image from the camera
-// TODO           mCamera?.takePicture(null, null, picture)
+            mCamera?.takePicture(null, null, picture)
         }
 
+    }
+
+//    override fun onPause() {
+//        super.onPause()
+//        releaseMediaRecorder() // if you are using MediaRecorder, release it first
+//        releaseCamera() // release the camera immediately on pause event
+//    }
+//
+//    private fun releaseMediaRecorder() {
+//        mediaRecorder?.reset() // clear recorder configuration
+//        mediaRecorder?.release() // release the recorder object
+//        mediaRecorder = null
+//        mCamera?.lock() // lock camera for later use
+//    }
+
+    private fun releaseCamera() {
+        mCamera?.release() // release the camera for other applications
+        mCamera = null
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -47,5 +86,62 @@ class CameraActivity : Activity() {
         }
     }
 
+    private val mPicture = Camera.PictureCallback { data, _ ->
+        val pictureFile: File =
+            getOutputMediaFile(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE) ?: run {
+                Log.d(TAG, ("Error creating media file, check storage permissions"))
+                return@PictureCallback
+            }
+
+        try {
+            val fos = FileOutputStream(pictureFile)
+            fos.write(data)
+            fos.close()
+        } catch (e: FileNotFoundException) {
+            Log.d(TAG, "File not found: ${e.message}")
+        } catch (e: IOException) {
+            Log.d(TAG, "Error accessing file: ${e.message}")
+        }
+    }
+
+    /** Create a file Uri for saving an image or video */
+    private fun getOutputMediaFileUri(type: Int): Uri {
+        return Uri.fromFile(getOutputMediaFile(type))
+    }
+
+    /** Create a File for saving an image or video */
+    private fun getOutputMediaFile(type: Int): File? {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        val mediaStorageDir = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+            "MyCameraApp"
+        )
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        mediaStorageDir.apply {
+            if (!exists()) {
+                if (!mkdirs()) {
+                    Log.d("MyCameraApp", "failed to create directory")
+                    return null
+                }
+            }
+        }
+
+        // Create a media file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        return when (type) {
+            MEDIA_TYPE_IMAGE -> {
+                File("${mediaStorageDir.path}${File.separator}IMG_$timeStamp.jpg")
+            }
+            MEDIA_TYPE_VIDEO -> {
+                File("${mediaStorageDir.path}${File.separator}VID_$timeStamp.mp4")
+            }
+            else -> null
+        }
+    }
 
 }
